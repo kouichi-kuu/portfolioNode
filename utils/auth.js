51c06ea7e71b7e2917860174618bfa6c9316cd05
kouichi-kuu <riosoulukou@gmail.com>
2024-01-20 20:01:38 +0900

    tokenの不具合を修正

--- utils/auth.js
+++ utils/auth.js
@@ -4,7 +4,7 @@ const auth = async(req,res,next)=>{
     if(req.method === "GET"){
         return next()
     }
-    const token = await req.headers.authorization.split("")[1]
+    const token = await req.headers.authorization.split(" ")[1]
     //const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJpb3NvdWx1a291QGdtYWlsLmNvbSIsImlhdCI6MTcwMDkxNDk2NCwiZXhwIjoxNzAwOTk3NzY0fQ.Wz9Hgb8lZLDrO7bhJ70y-h5E0mdLsGnvxnHURa-WAcw"
     if(!token){
         return res.status(400).json({message:"トークンがありません"})
